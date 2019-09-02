4798c72f320bc94d8835fabb23090277a4b772dc
Flemming Meyer <fm@indexed.dk>
2019-09-02 19:55:28 +0200

    tt

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -9,7 +9,7 @@
     <p>
       Lorem ipsum dolor sit amet, consectetur adipisicing elit. Pariatur excepturi labore tempore expedita, et iste tenetur suscipit explicabo! Dolores, aperiam non officia eos quod asperiores
     </p>
-    <img src="/uploads/test.jpg" alt="">
+    <img src="/uploads/test.jpg?nf_resize=fit&w=1000" alt="">
     <p class="home-links">
       <a href="https://gridsome.org/docs" target="_blank" rel="noopener">Gridsome Docs</a>
       <a href="https://github.com/gridsome/gridsome" target="_blank" rel="noopener">GitHub</a>
